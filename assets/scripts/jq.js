de2bc01b2f50e21820a2d471250abc1e44f9cd11
bmlinch <bmlinch7266@gmail.com>
2016-01-28 11:47:35 -0700

    OMFG

--- assets/scripts/jq.js
+++ assets/scripts/jq.js
@@ -1,4 +1,4 @@
-var cId= 0;
+var cId = 0;
 var newPlayerForm = $('#newPlayer')
 var roster = [];
 function addPlayer(e) {
@@ -17,50 +17,65 @@ function addPlayer(e) {
     cId++
 }
 
-// function draw(obj) {
-//     $(".player-roster").prepend("<div class='player-card'></div>")
-//     var pcard = $(".player-roster").find(".player-card")[0];
-//     pcard = $(pcard)
-//     // pcard(".po").append('<img class="po" src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="This.">')
-//     // $('#addname').append(obj.name + " ")
-//     // $('#addposition').append(obj.position + " ")
-//     // $('#addnumber').append(obj.num + " ")
-   
-//    pcard.append('<img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="Profile Picture">')
-//    pcard.append('<button type="button" class="btn btn-danger" id="'+obj.id+'">Danger</button>')
-//    pcard.append('<p>' + obj.name + '</p>')
-//    pcard.append('<p>' + obj.position + '</p>')
-//    pcard.append('<p>' + obj.num + '</p>')
-    
-// }
 
 function draw(obj) {
-	var playerRoster = $(".player-roster");
-	var pcard = $("<div class='player-card'></div>")
-	pcard.append('<img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="Profile Picture">')
-	pcard.append('<button type="button" class="btn btn-danger remove">Danger</button>')
-	pcard.append('<p>' + obj.name + '</p>')
-	pcard.append('<p>' + obj.position + '</p>')
-	pcard.append('<p>' + obj.num + '</p>')
-	
-	pcard.find('button.remove').click(function(){
-           
-        
-		//delete player from the list of players at some point...
-        for (var i = 0; i < roster.length; i++){
-            debugger;
-        
-            if(this.id == roster[i].id){
-                alert(roster[i].name)
-                // roster[i].remove()
-            }
-        }
-		playerRoster.remove(pcard);
-	})
-	
-	playerRoster.prepend(pcard);	
+    var playerRoster = $(".player-roster");
+    var pcard = $("<div class='player-card' id=" + obj.id + "></div>")
+    pcard.append('<img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="Profile Picture">')
+    pcard.append('<button type="button" class="btn btn-danger remove">Danger</button>')
+    pcard.append('<p>' + obj.name + '</p>')
+    pcard.append('<p>' + obj.position + '</p>')
+    pcard.append('<p>' + obj.num + '</p>')
+    playerRoster.prepend(pcard);
+
+
 }
-newPlayerForm.submit(addPlayer)
+
+$('.player-roster').on('click', ".btn-danger", removePlayer)
+
+
+
+
+function getIndex(id) {
+
+    for (var i = 0; i < roster.length; i++) {
+        debugger
+        if (id === roster[i].id) {
+            return i;
+        }
+    }
+    return -1
+}
+
+function removePlayer(e) {
+    // var id = $(".player-card").attr("id")
+    
+    var btn = $(e.target);
+    var card = btn.closest(".player-card")
+    var myId = parseInt(card.attr("id"))
+    debugger
+    roster.forEach(function (player, i) {
+        if (player.id === myId) {
+            roster.splice(i, 1)
+
+        }
+    })
+    card.remove()
+    $(".player-roster").empty()
+    reDraw()
+}
+
+
+
+function reDraw() {
+    // var rosterElem = $('.player-roster')
+    for (var i = 0; i < roster.length; i++) {
+        draw(roster[i])
+    }
+
+
+}
+newPlayerForm.submit(addPlayer);
 
 // $('#reset').click(resett)
 
